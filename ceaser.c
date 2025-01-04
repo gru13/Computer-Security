6727e3646a2392a454da6f61fb11a3e9b6f8b7dc
Guruprasath <44829747+gru13@users.noreply.github.com>
2025-01-04 09:44:03 +0000

    j

--- ceaser.c
+++ ceaser.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #define BUFFER_SIZE 1024
 
 int main() {
@@ -9,25 +10,30 @@ int main() {
 
     printf("Enter the Key: ");
     scanf("%d", &key);
+
     printf("Enter the Text (plain in lower and Cipher in upper): ");
     scanf("%s", input);
 
     int len = strlen(input);
     int i = 0;
-    output[len] = '\0'; 
 
-    if (input[0] >= 'a' && input[0] <= 'z') {
-        while (input[i] != '\0') {
-            output[i] = (((int)input[i] - (int)'a' + key) % 26) + 'A'; 
-            i++;
+    for (i = 0; i < len; i++) {
+        if (islower(input[i])) {  // Encrypt lowercase to uppercase
+            output[i] = (((input[i] - 'a') + key) % 26) + 'A';
+        } else if (isupper(input[i])) {  // Decrypt uppercase to lowercase
+            output[i] = (((input[i] - 'A') + key) % 26) + 'a';
+        } else {  // Invalid character in input
+            printf("Invalid character in input: %c\n", input[i]);
+            return 1;
         }
-        printf("The Cipher text: %s\n", output); 
-    } else {  
-        while (input[i] != '\0') {
-            output[i] = (((int)input[i] - (int)'A' + key) % 26) + 'a'; 
-            i++;
-        }
-        printf("The Plain text: %s\n", output);  
+    }
+
+    output[len] = '\0';  // Null-terminate the output string
+
+    if (islower(input[0])) {
+        printf("The Cipher text: %s\n", output);
+    } else {
+        printf("The Plain text: %s\n", output);
     }
 
     return 0;
